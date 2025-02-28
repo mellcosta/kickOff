d88df272df3c1a0173ca4a97d6cb1a45e6bfa9f4
mellcosta <m3licosta@gmail.com>
2025-02-28 15:12:10 +0530

    New word

--- src/Components/SingTheWord.jsx
+++ src/Components/SingTheWord.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 const words = [
   "Grace", "Hallelujah", "Jesus", "Faith", "Glory", 
-  "Savior", "Praise", "Mercy", "Hope", "Redeemed",
-  "Victory", "Worship", "Deliverance", "Salvation"
+  "Savior", "Praise", "Mercy", "Hope", "Blood",
+  "Victory", "Worship", "Day", "Salvation"
 ];
 
 const SingTheWord = () => {
